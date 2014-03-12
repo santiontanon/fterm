5784324180d4180414373782047feda73403eede
santi.ontanon@gmail.com <santi.ontanon@gmail.com@bbb28d61-803d-14aa-c3be-b90e241c983c>
2014-03-12 21:19:47 +0000

    removed all the source code to replace it by the new version of FTL

--- src/fterms/subsumption/SubsumptionBackTrackNode.java
+++
@@ -1,9 +0,0 @@
-package fterms.subsumption;
-
-import java.util.List;
-
-public class SubsumptionBackTrackNode {
-	int n_bindings;
-	List<SubsumptionStackNode> m_stack;
-	SubsumptionStackNode m_node;
-}
